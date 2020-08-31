fc7dce666f48874cf52c2f44c4467826dfc6dbb1
WepILoK <artur.nasyrov1999@mail.ru>
2020-08-31 13:14:13 +0300

    Updates

---
+++ precache-manifest.6c06f352d0a2db3d66436bdf22944f50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dde8ca36f019bb871f9e5c1c48d9388b",
+    "url": "/react-ts-todo/index.html"
+  },
+  {
+    "revision": "6e884d5d9322e68f9c0c",
+    "url": "/react-ts-todo/static/css/main.46adbcf4.chunk.css"
+  },
+  {
+    "revision": "9f60967e2677689ed75d",
+    "url": "/react-ts-todo/static/js/2.b746c6ae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-ts-todo/static/js/2.b746c6ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e884d5d9322e68f9c0c",
+    "url": "/react-ts-todo/static/js/main.f0802f8a.chunk.js"
+  },
+  {
+    "revision": "d8eefd71663950ca5dd9",
+    "url": "/react-ts-todo/static/js/runtime-main.6f260a4f.js"
+  }
+]);
